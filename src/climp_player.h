e72fd84fc77385a962dfcb29457b04b24015f0b7
stnuessl <bockler@gmx.net>
2014-02-15 01:32:17 +0100

    Added GPL Header.

--- src/climp_player.h
+++ src/climp_player.h
@@ -1,3 +1,21 @@
+/* 
+ * climp - Command Line Interface Music Player
+ * Copyright (C) 2014  Steffen Nüssle
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _CLIMP_PLAYER_H_
 #define _CLIMP_PLAYER_H_
 
